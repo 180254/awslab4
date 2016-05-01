42c4c9cebf3ad0b4c43b68f2e13014d54bc16bfa
Adrian Pędziwiatr <180254@edu.p.lodz.pl>
2016-05-01 21:34:28 +0200

    done(store file hashes & from access log in SimpleDb)

--- actions/createform.js
+++ actions/createform.js
@@ -6,6 +6,18 @@ var AWS_CONFIG_FILE = "config.json";
 var POLICY_FILE = "policy.json";
 var INDEX_TEMPLATE = "index.ejs";
 
+var AWS = require("aws-sdk");
+AWS.config.loadFromPath('./config.json');
+var simpledb = new AWS.SimpleDB();
+
+// credits: friends @ stackoverflow
+// http://stackoverflow.com/posts/2117523/revisions
+var uuid = function () {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
+};
 
 var task = function(request, callback){
 	//1. load configuration
@@ -27,7 +39,17 @@ var task = function(request, callback){
 	s3Form.addField(s3Fields, 'x-amz-meta-my-name', 'Adrian Pedziwiatr');
 	s3Form.addField(s3Fields, 'x-amz-meta-uploaded-by', request.ip);
 
-	callback(null, {template: INDEX_TEMPLATE, params:{fields:s3Fields, bucket:bucketName}});
-}
+    var logText = new Date().toLocaleString() + ' | ' +request.ip;
+    var params = {
+        DomainName: 'koszykadi',
+        ItemName: '___FormAccessLog',
+        Attributes: [ { Name: uuid(), Value:logText } ]
+    };
+    simpledb.putAttributes(params, function(err, data) {
+        if (err) callback(null, err.stack);
+        else 	callback(null, {template: INDEX_TEMPLATE, params:{fields:s3Fields, bucket:bucketName}});
+    });
+
+};
 
 exports.action = task;
--- actions/uploaded.js
+++ actions/uploaded.js
@@ -4,7 +4,9 @@ var helpers = require("../helpers");
 
 var AWS = require("aws-sdk");
 AWS.config.loadFromPath('./config.json');
+
 var s3 = new AWS.S3();
+var simpledb = new AWS.SimpleDB();
 
 var task = function (request, callback) {
 
@@ -14,38 +16,51 @@ var task = function (request, callback) {
 
     var filePath = 'bucketfiles/' + key.replace(/[^0-9a-zA-Z_\.]/g, '_');
 
-    s3.getObject(
-        { Bucket: bucket, Key: key },
-        function (err, data) {
-            if (err) callback(null, err.stack);
-            else {
-                fs.writeFile(filePath, data.Body);
+    s3.getObject({ Bucket: bucket, Key: key }, function (err, data) {
+        if (err) callback(null, err.stack);
+        else {
+            fs.writeFile(filePath, data.Body);
 
-                var info = {};
-                info[ 'bucket' ] = bucket;
-                info[ 'bucket_key' ] = key;
-                info[ 'filePath_local' ] = filePath;
+            var metadata = data.Metadata;
 
-                var digests = {};
-                digests[ 'md5' ] = helpers.calculateDigest("md5", data.Body, 'hex');
-                digests[ 'sha1' ] = helpers.calculateDigest("sha1", data.Body, 'hex');
-                digests[ 'sha256' ] = helpers.calculateDigest("sha256", data.Body, 'hex');
-                digests[ 'sha512' ] = helpers.calculateDigest("sha512", data.Body, 'hex');
+            var info = {};
+            info[ 'bucket' ] = bucket;
+            info[ 'bucket_key' ] = key;
+            info[ 'filePath_local' ] = filePath;
 
-                console.log(data.Metadata);
-                callback(null, {
-                    template: 'uploaded.ejs',
-                    params: {
-                        info: info,
-                        digests: digests,
-                        metadata: data.Metadata
-                    }
-                });
-            }
+            var digests = {};
+            digests[ 'md5' ] = helpers.calculateDigest("md5", data.Body, 'hex');
+            digests[ 'sha1' ] = helpers.calculateDigest("sha1", data.Body, 'hex');
+            digests[ 'sha256' ] = helpers.calculateDigest("sha256", data.Body, 'hex');
+            digests[ 'sha512' ] = helpers.calculateDigest("sha512", data.Body, 'hex');
 
+            var paramsSimpleDb = {
+                DomainName: 'koszykadi',
+                ItemName: 'key',
+                Attributes: [
+                    { Name: 'md5', Value: digests[ 'md5' ] },
+                    { Name: 'sha1', Value: digests[ 'sha1' ] },
+                    { Name: 'sha256', Value: digests[ 'sha256' ] },
+                    { Name: 'sha512', Value: digests[ 'sha512' ] }
+                ]
+            };
+
+            simpledb.putAttributes(paramsSimpleDb, function (err, data) {
+                if (err) callback(null, err.stack);
+
+                else {
+                    callback(null, {
+                        template: 'uploaded.ejs',
+                        params: {
+                            info: info,
+                            digests: digests,
+                            metadata: metadata
+                        }
+                    });
+                }
+            });
         }
-    );
-
+    });
 };
 
 exports.action = task;
